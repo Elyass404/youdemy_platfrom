de6d7cbfc0382ae61bf51342624bf5d88e25faa1
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-01-20 04:09:25 +0100

    finished the process of accepting the course by the admin

--- controllers/courses/acceptCourseCtrl.php
+++ controllers/courses/acceptCourseCtrl.php
@@ -16,16 +16,18 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
     $courseId = $_GET['id'];
     $adminObj = new Admin($db);
     if($adminObj->acceptCourse($courseId)){
-        echo 'the course hass been accepted successfully';
+        $_SESSION['message'] = "Course accepeted successfully.";
+                header('Location:../../views/admin/courses.php');
+                exit;
+    }else {
+        $_SESSION['message'] = "Failed to complete the operation.";
+        header('Location:../../views/admin/courses.php');
+        exit;
     }
+}else{
+    $_SESSION['message'] = "Course name is required.";
+    header('Location:../../views/admin/categories.php');
+    exit;
 }
-
-
-
-
-
-
-
-
 
 ?>
